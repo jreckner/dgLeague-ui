50e423d66bb154cfcb7b0c0511aa1d97f1bd2f3c
Jon Reckner <jreckner@lexmark.com>
2017-10-20 13:43:30 -0400

    Update to Angular 4.4.x

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,26 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-// libraries
-import { Ng2BootstrapModule } from 'ng2-bootstrap';
-
 // root component
 import { AppComponent } from './app.component';
 
 // internal Routes
 import { APP_ROUTER } from './app.routes';
-import { FooterComponent } from "./components/footer.component";
-import { HeaderComponent } from "./components/header.component"
+import { FooterComponent } from './components/footer.component';
+import { HeaderComponent } from './components/header.component';
 
 @NgModule({
   imports: [
-    // angular
     BrowserModule,
     ReactiveFormsModule,
     HttpModule,
-    // libraries
-    Ng2BootstrapModule,
-    // internal
     APP_ROUTER
   ],
   declarations: [
